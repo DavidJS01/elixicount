35a0204c529ffe5bba3a1d08b11ba9b9af6afe34
DavidJS01 <davidshipmanchs2020@gmail.com>
2023-06-01 21:34:47 -0600

    feat: add changeset validator on food_servings schema (I learned a lot!)

--- lib/elixicount/schemas/food_serving.ex
+++ lib/elixicount/schemas/food_serving.ex
@@ -1,5 +1,6 @@
 defmodule Elixicount.Schemas.FoodServing do
   use Ecto.Schema
+  import Ecto.Changeset
 
   schema "food_servings" do
     field :food_name, :string
@@ -7,4 +8,13 @@ defmodule Elixicount.Schemas.FoodServing do
     field :serving_quantity, :integer
     field :units, :integer
   end
+
+  def changeset(struct, params) do
+    struct
+    |> cast(params, [:food_name, :calories_per_serving, :serving_quantity, :units])
+    |> validate_required([:food_name, :calories_per_serving, :serving_quantity, :units])
+    |> validate_number(:calories_per_serving, greater_than: 0)
+    |> validate_number(:serving_quantity, greater_than: 0)
+  end
+  # @TODO: explore why validate number is failing
 end
